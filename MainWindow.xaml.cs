fe07ab70530675ff1e6e947dfb2c70c778e487e6
agab0a <127765215+agab0a@users.noreply.github.com>
2025-06-14 08:54:09 +0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        QlbenhNhanContext db = new QlbenhNhanContext();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dgvHienThi.ItemsSource = (from bn in db.BenhNhans
+                                      where bn.SongayNv <= 20
+                                      orderby bn.SongayNv descending
+                                      select new
+                                      {
+                                          bn.Mabn,
+                                          bn.Hoten,
+                                          bn.Diachi,
+                                          bn.SongayNv,
+                                          bn.Makhoa,
+                                          VienPhi = bn.SongayNv * 60000
+                                      }).ToList();
+
+        }
+    } 
+}
